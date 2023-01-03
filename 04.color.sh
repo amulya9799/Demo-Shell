f979225d1841aa8320d98b4cc89628dfc764418e
amulya9799 <amulya1080@gmail.com>
2023-01-03 12:09:36 +0000

    sa

---
+++ 04.color.sh
@@ -0,0 +1,7 @@
+#for red colour
+echo -e "\e[31mHELLO\e[0m"
+echo -e "\e[32mHELLO\e[0m"
+echo -e "\e[33mHELLO\e[0m"
+echo -e "\e[34mHELLO\e[0m"
+echo -e "\e[35mHELLO\e[0m"
+echo -e "\e[36mHELLO\e[0m"
