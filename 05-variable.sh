45283a47024df3fab7168462052bcce2b40217c6
amulya9799 <amulya1080@gmail.com>
2023-01-03 14:18:53 +0000

    aas

--- 05-variable.sh
+++ 05-variable.sh
@@ -2,4 +2,9 @@ trainer=raghu
 class=devops
 
 echo trainer name-$trainer
-echo class name-${class}
+echo class name-$class
+Add=$((2+3))
+echo Add - $Add
+DATE=$(date +%F)
+echo hey today date is $DATE
+echo b - $b
